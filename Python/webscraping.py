756fd99197af7cb081764e54ebfd37e1f297faac
Schumarc <ps3-zocker-1996@web.de>
2019-05-03 20:03:55 +0200

    URL hinzugefügt

--- Python/webscraping.py
+++ Python/webscraping.py
@@ -1,5 +1,8 @@
 from urllib import request 
+import time
+
 url_requested = request.urlopen("https://www.tipico.de/de/online-sportwetten/fussball/deutschland/bundesliga/g42301/")
 if(url_requested.code == 200):
     html_content = str(url_requested.read())
     print(html_content)
+    time.sleep(100)
