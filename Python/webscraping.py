41e78f7ebb5798729b0cf21d6ab07f9817f3f3ac
Schumarc <ps3-zocker-1996@web.de>
2019-05-03 20:18:24 +0200

    HTML schreiben

--- Python/webscraping.py
+++ Python/webscraping.py
@@ -1,8 +1,10 @@
 from urllib import request 
 import time
+import re
 
+page_content=[]
 url_requested = request.urlopen("https://www.tipico.de/de/online-sportwetten/fussball/deutschland/bundesliga/g42301/")
 if(url_requested.code == 200):
     html_content = str(url_requested.read())
-    print(html_content)
-    time.sleep(100)
+    file = open('test.txt','w') 
+    file.write(html_content)
